7b390bc70b8771767b3a2d72a11949d18a671b76
Henrique Garcia <henriquegarcia.dev@gmail.com>
2024-11-20 12:50:06 -0300

    feat(frontend): Integração do funcionamento do componente SideMenu

--- src/components/layout/SideMenu.tsx
+++ src/components/layout/SideMenu.tsx
@@ -1,24 +1,77 @@
 'use client'
 
 import clsx from 'clsx'
+import { Button } from '@/components'
 
 import { useViews } from '@/contexts/ViewsProvider'
+
+import { IView, viewsData } from '@/data/views'
+import { useEffect, useState } from 'react'
 
 interface ISideMenuProps {}
 
 const SideMenu: React.FC<ISideMenuProps> = ({}) => {
-  const { isSideMenuOpen } = useViews()
+  const { activeView, handleChangeActiveView, isSideMenuOpen } = useViews()
+  const [showMenuLabels, setShowMenuLabels] = useState(false)
+
+  useEffect(() => {
+    if (isSideMenuOpen) {
+      const timeout = setTimeout(() => setShowMenuLabels(true), 200)
+      return () => clearTimeout(timeout)
+    } else {
+      setShowMenuLabels(false)
+    }
+  }, [isSideMenuOpen])
 
   return (
-    <div
-      className={clsx(
-        'fixed top-admin-header left-0 flex h-admin-content border-r border-r-border-primary bg-foreground transition-all duration-300',
-        {
-          'w-admin-sidemenu': !isSideMenuOpen,
-          'w-admin-sidemenu-open': isSideMenuOpen
-        }
+    <div className="fixed top-admin-header left-0 flex w-full h-admin-content">
+      <div
+        className={clsx(
+          '!z-[100] fixed top-admin-header left-0 flex h-admin-content border-r border-r-border-primary bg-foreground transition-all duration-200',
+          {
+            'w-admin-sidemenu': !isSideMenuOpen,
+            'w-admin-sidemenu-open': isSideMenuOpen
+          }
+        )}
+      >
+        <nav className="flex flex-col gap-[10px] w-full p-[10px]">
+          {viewsData.map((menu: IView) => {
+            if (!menu.visible) return null
+            const isActive = menu.path === activeView
+
+            return (
+              <Button
+                key={menu.id}
+                active={isActive}
+                onClick={() => handleChangeActiveView(menu.path)}
+                disabled={!menu.active}
+                variant="menu"
+                size="menu"
+                className="relative"
+              >
+                {menu.icon}
+                {showMenuLabels && (
+                  <span
+                    className={clsx(
+                      'pt-[1px] transition-opacity duration-200',
+                      {
+                        'opacity-0': !isSideMenuOpen,
+                        'opacity-100': isSideMenuOpen
+                      }
+                    )}
+                  >
+                    {menu.label}
+                  </span>
+                )}
+              </Button>
+            )
+          })}
+        </nav>
+      </div>
+      {isSideMenuOpen && (
+        <div className="!z-[10] absolute top-0 left-0 flex w-full h-admin-content backdrop-blur-[2px]"></div>
       )}
-    ></div>
+    </div>
   )
 }
 
--- src/components/ui/Button.tsx
+++ src/components/ui/Button.tsx
@@ -5,24 +5,27 @@ import { cva, type VariantProps } from 'class-variance-authority'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-[22px] [&_svg]:shrink-0 transition-all duration-200',
+  'inline-flex items-center justify-center gap-[8px] whitespace-nowrap rounded-md text-sm font-medium ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:shrink-0 transition-all duration-200',
   {
     variants: {
       variant: {
         default: 'bg-primary text-primary-foreground hover:bg-primary/90',
         destructive:
           'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'bg-foreground hover:bg-accent hover:text-accent-foreground',
+        outline:
+          'bg-foreground hover:bg-accent hover:text-accent-foreground [&_svg]:size-[22px]',
         secondary:
           'bg-secondary text-secondary-foreground hover:bg-secondary/80',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
-        link: 'text-primary underline-offset-4 hover:underline'
+        link: 'text-primary underline-offset-4 hover:underline',
+        menu: 'justify-start pl-[14px] text-[14px] bg-foreground text-color-secondary hover:bg-accent [&_svg]:size-[16px]'
       },
       size: {
         default: 'h-10 px-4 py-2',
         sm: 'h-9 rounded-md px-3',
         lg: 'h-11 rounded-md px-8',
-        icon: 'h-[40px] w-[40px]'
+        icon: 'h-[40px] w-[40px]',
+        menu: 'h-[40px] w-full'
       }
     },
     defaultVariants: {
@@ -44,10 +47,12 @@ const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
     const Comp = asChild ? Slot : 'button'
     return (
       <Comp
-        className={cn(
-          buttonVariants({ variant, size, className }),
-          active && 'bg-accent hover:bg-accent hover:text-accent-foreground'
-        )}
+        className={cn(buttonVariants({ variant, size, className }), {
+          'bg-accent hover:bg-accent hover:text-accent-foreground':
+            active && variant === 'outline',
+          'bg-active-primary text-color-secondary hover:bg-active-primary hover:text-accent-foreground':
+            active && variant === 'menu'
+        })}
         ref={ref}
         {...props}
       />
